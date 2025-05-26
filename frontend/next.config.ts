fe7ad69fc55b3c742b68d91032adc6b781ed3a36
RajyavardhanSingh <rajyavardhansing2003@gmail.com>
2025-05-27 00:50:55 +0530

    Integrate frontend files into main repository

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
- experimental: {
-    // Disable scroll restoration (which can cause animation issues)
-    scrollRestoration: false,
-  },
-  // Add this to prevent unwanted redirects during hydration
-  reactStrictMode: false,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
   },
 };
 
